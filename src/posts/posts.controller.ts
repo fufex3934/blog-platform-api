23e41b967c882b84d6e632aeb4824f92471c0fe8
Fufa Wakgari <fufawakagari174@gmail.com>
2026-03-10 15:51:56 +0300

    implement post controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postService: PostsService) {}
+
+  @Post()
+  async create(@Body() createPostDto: CreatePostDto) {
+    return this.postService.create(createPostDto);
+  }
+
+  @Get()
+  async findAll(@Query('page') page = 1, @Query('limit') limit = 10) {
+    return this.postService.findAll(Number(page), Number(limit));
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.postService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
+    return this.postService.update(id, updatePostDto);
+  }
+
+  @Delete(':id')
+  async delete(id: string) {
+    return this.postService.delete(id);
+  }
+}
