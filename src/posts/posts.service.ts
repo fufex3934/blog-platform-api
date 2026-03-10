8326c5745922295475cf02e15261bd145599c631
Fufa Wakgari <fufawakagari174@gmail.com>
2026-03-10 15:45:26 +0300

    implement post service

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post, PostDocument } from './schemas/post.schema';
+import { Model } from 'mongoose';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(@InjectModel(Post.name) private postModel: Model<PostDocument>) {}
+
+  async create(createPostDto: CreatePostDto): Promise<Post> {
+    const newPost = new this.postModel(createPostDto);
+    return await newPost.save();
+  }
+
+  async findAll(page = 1, limit = 10) {
+    const skip = (page - 1) * limit;
+
+    const posts = await this.postModel.find().skip(skip).limit(limit).sort({
+      createdAt: -1,
+    });
+
+    return posts;
+  }
+
+  async findOne(id: string) {
+    const post = await this.postModel.findById(id);
+    if (!post) {
+      throw new NotFoundException('post not found');
+    }
+    return post;
+  }
+
+  async update(id: string, updatePostDto: UpdatePostDto) {
+    const post = await this.postModel.findByIdAndUpdate(id, updatePostDto, {
+      new: true,
+    });
+
+    if (!post) {
+      throw new NotFoundException('post not found');
+    }
+    return post;
+  }
+
+  async delete(id: string) {
+    const post = await this.postModel.findByIdAndDelete(id);
+
+    if (!post) {
+      throw new NotFoundException('post not found');
+    }
+    return { message: 'Post deleted successfully' };
+  }
+}
