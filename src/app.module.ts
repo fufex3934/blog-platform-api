2372bf9c9d45dd291fd2732a918d2c03d509c0e9
Fufa Wakgari <fufawakagari174@gmail.com>
2026-03-11 09:22:45 +0300

    caching

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostsModule } from './posts/posts.module';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/blog-platform'),
+    CacheModule.register({
+      ttl: 10,
+      max: 100,
+      isGlobal: true,
+    }),
     PostsModule,
   ],
   controllers: [AppController],
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,13 +1,18 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Post, PostDocument } from './schemas/post.schema';
 import { Model } from 'mongoose';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import * as cacheManager from 'cache-manager';
 
 @Injectable()
 export class PostsService {
-  constructor(@InjectModel(Post.name) private postModel: Model<PostDocument>) {}
+  constructor(
+    @InjectModel(Post.name) private postModel: Model<PostDocument>,
+    @Inject(CACHE_MANAGER) private cacheManager: cacheManager.Cache,
+  ) {}
 
   async create(createPostDto: CreatePostDto): Promise<Post> {
     const newPost = new this.postModel(createPostDto);
@@ -15,11 +20,21 @@ export class PostsService {
   }
 
   async findAll(page = 1, limit = 10) {
+    const cacheKey = `posts-${page}-${limit}`;
+
+    const cachedPosts = await this.cacheManager.get(cacheKey);
+
+    if (cachedPosts) {
+      console.log('serving from cache');
+      return cachedPosts;
+    }
     const skip = (page - 1) * limit;
 
     const posts = await this.postModel.find().skip(skip).limit(limit).sort({
       createdAt: -1,
     });
+
+    await this.cacheManager.set(cacheKey, posts);
 
     return posts;
   }
