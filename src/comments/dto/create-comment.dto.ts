414feadf2447f22be36a5f8c7b5e36ea87617b40
Fufa Wakgari <fufawakgari174@gmail.com>
2026-03-13 11:36:44 +0300

    Update CreateCommentDto

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,7 +1,10 @@
-import { IsString, MinLength } from 'class-validator';
+import { IsOptional, IsString, MinLength } from 'class-validator';
 
 export class CreateCommentDto {
   @IsString()
   @MinLength(2)
   content: string;
+
+  @IsOptional()
+  parentId?: string;
 }
